7b741fcf962c7db27fa4aa90abad318e72d81897
ruslyrossi <rusly@Ruslys-MacBook-Air-2.local>
2023-01-31 23:09:02 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./dist/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'blue-google': '#4285f4',
+        'blue-google-dark': '#1a73e8',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
